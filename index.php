6d702bd3af186bd1d98a55b3614d9dc72809ae12
errajkumarthakur <124517442+errajkumarthakur@users.noreply.github.com>
2023-03-07 14:44:56 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Untitled Document</title>
+<form method="get" action="save.php">
+</head>
+
+<body bgcolor="#CCCCFF">
+<table width="257" border="0" align="center" bgcolor="#6699FF">
+  <tr>
+    <td width="95">name</td>
+    <td width="152"><input type="text" name="nme" /></td>
+  </tr>
+  <tr> 
+    <td>class</td>
+    <td><input type="text" name="cls" /></td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td><input type="submit" name="Submit" value="Display" /></td>
+  </tr>
+</table>
+ </form>
+</body>
+</html>
---
+++ save.php
@@ -0,0 +1,19 @@
+<?php
+$name=$_GET["nme"];
+$class=$_GET["cls"];
+?> 
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Untitled Document</title>
+</head>
+
+<body bgcolor="#336699">
+<?php
+echo "name : ".$name;
+echo"<br>";
+echo " class : ".$class; 
+?>
+</body>
+</html>
